e6fd80bec35064c1f4cfdbf32acd8c2f87c28e00
Doug Benjamin <dougbenjaminatwork@gmail.com>
2023-09-12 12:46:41 -0500

    add new function to init script

--- dask_sharedpool/__init__.py
+++ dask_sharedpool/__init__.py
@@ -4,7 +4,7 @@ _logger = _logging.getLogger(__name__)
 _logger.setLevel(_logging.DEBUG)
 _logger.addHandler(_logging.NullHandler())
 
-from .cluster import Bnlt3Cluster
+from .cluster import Bnlt3Cluster, get_free_dask_scheduler_port
 from .config import _ensure_user_config_file, _set_base_config
 
 _ensure_user_config_file()
